30338f9bbfdd3acde085a4edd49b03c4d40a4f6c
Kristian Gregorius Hustad <kghustad@simula.no>
2021-06-02 19:26:30 +0200

    src/c/cost_terms.h: clang-format

--- src/c/cost_terms.h
+++ src/c/cost_terms.h
@@ -11,6 +11,6 @@ typedef int (*progress_update_func_ptr)(int);
 #define NUM_COST_TERMS 60
 
 void CDECL all_cost_terms(double *R, double *traces, double *t, uint8_t *mask, long length,
-                    long num_parameter_sets, progress_update_func_ptr progress_update);
+                          long num_parameter_sets, progress_update_func_ptr progress_update);
 
 #endif
