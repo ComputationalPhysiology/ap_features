216f324e999550354c7a34a1e7f4370f0ef97d0c
Henrik Finsberg <henriknf@simula.no>
2022-06-17 16:00:39 +0200

    Bump version: 2022.3.5 → 2022.3.6

--- setup.py
+++ setup.py
@@ -151,5 +151,5 @@ requirements = ["numpy", "numba", "tqdm"]
 
 setup(
     ext_modules=[get_extension()],
-    version="2022.3.5",
+    version="2022.3.6",
 )
--- src/python/ap_features/__init__.py
+++ src/python/ap_features/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Henrik Finsberg"""
 __email__ = "henriknf@simula.no"
-__version__ = "2022.3.5"
+__version__ = "2022.3.6"
 
 import logging as _logging
 
